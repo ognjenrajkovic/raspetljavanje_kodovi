2f8315ceca294502dedb0f1e79033c5a6441d5ad
ognjenrajkovic <141023658+ognjenrajkovic@users.noreply.github.com>
2024-12-11 19:01:28 +0100

    Delete domaci/domaci-druga-lekcija/domaci1/domaci1 directory

--- domaci/domaci-druga-lekcija/domaci1/domaci1/zadatak6.py
+++
@@ -1,8 +0,0 @@
-budzet = int(input())
-broj_dana = int(input())
-troskovi = 100
-
-if troskovi * broj_dana <= budzet:
-    print("Da")
-else:
-    print("Ne")
--- domaci/domaci-druga-lekcija/domaci1/domaci1/zadatak8.py
+++
@@ -1,7 +0,0 @@
-V = int(input())
-x = int(input())
-y = int(input())
-
-broj_sati = V // (x + y)
-
-print(broj_sati)
