d04fbb329233f71b9bb1740ec5726ae7efdd61ea
ognjenrajkovic <141023658+ognjenrajkovic@users.noreply.github.com>
2024-12-11 19:27:27 +0100

    Delete zadaci
    Brisanje zadataka, pošto nisu uplodovani u dobar folder

--- domaci/domaci-druga-lekcija/domaci-prva-lekcija/zadatak4.py
+++
@@ -1,9 +0,0 @@
-broj = int(input())
-
-zadnja = broj % 10
-sredisnja = broj % 100 // 10
-prva = broj % 1000 // 100
-
-zbir = zadnja + sredisnja + prva
-
-print(zbir)
--- domaci/domaci-druga-lekcija/domaci-prva-lekcija/zadatak5.py
+++
@@ -1,8 +0,0 @@
-broj = int(input())
-
-zadnja = broj % 10
-treca = broj % 100 // 10
-druga = broj % 1000 // 100
-prva = broj % 10000 // 1000
-
-print(zadnja, treca, druga, prva, sep="")
